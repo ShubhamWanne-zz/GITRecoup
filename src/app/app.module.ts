e72a72e70563f6b18a73ceefdd32202a82abd69b
Shubham Wanne <shubhamwanne5@gmail.com>
2018-11-29 19:46:02 +0530

    User can view Graphical representaion of repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
-import {FormsModule} from "@angular/forms"
+import {FormsModule, ReactiveFormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 import { OperationComponent } from './operation/operation.component';
@@ -14,6 +14,7 @@ import { OperationComponent } from './operation/operation.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule ,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
--- src/app/operation/operation.component.ts
+++ src/app/operation/operation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+import {ColorGenerator} from '../../Color';
 
 @Component({
   selector: 'app-operation',
@@ -7,30 +8,76 @@ import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrie
   styleUrls: ['./operation.component.scss']
 })
 export class OperationComponent implements OnInit {
-  CLIENT_ID:string = "02105ca1007bab3db720";
-  CLIENT_SECRET:string = "933490fc379f175ce0cd89f093c9aca3a5cade37";
-  isSubmitClicked:boolean = false;
-  isInvalidUser:boolean = false;
-  isShowRepositories:boolean = false;
-  showRepoButtonTag:string = "Show Repositories";
-  userName:string;
-  URL:string;
-  repository:string;
-  company:string;
-  userDetails:any;
+  CLIENT_ID: string = "02105ca1007bab3db720";
+  CLIENT_SECRET: string = "933490fc379f175ce0cd89f093c9aca3a5cade37";
+  isSubmitClicked: boolean = false;
+  isInvalidUser: boolean = false;
+  isShowRepositories: boolean = false;
+  showRepoButtonTag: string = "Show Repositories";
+  isShowChart: boolean = false;
+  showChartButtonTag: string = "View";
+  userName: string;
+  URL: string;
+  repository: string;
+  company: string;
+  userDetails: any;
   repoData: any[];
   followers: any[];
   followersDetails = new Map();
-  constructor() { }
+  colorGenerator = new ColorGenerator();
+
+  /* 
+  ---- Chart Details
+  */
+  isChartCreated: boolean = false;
+  public chartType: string = 'polarArea';
+  public chartData= new Array();
+  public chartLabels= new Array();
+  public backgroundColor= new Array();
+  public chartColors: Array<any> = [{
+    hoverBorderColor: ['rgba(0, 0, 0, 0.1)', 'rgba(0, 0, 0, 0.1)', 'rgba(0, 0, 0, 0.1)', 'rgba(0, 0, 0, 0.1)', 'rgba(0, 0, 0, 0.1)'],
+    hoverBorderWidth: 0,
+  }];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
-  fetchUser = function(form:any){
+  resetData= function(){
+    isSubmitClicked: false;
+    isInvalidUser: false;
+    isShowRepositories: false;
+    showRepoButtonTag: "Show Repositories";
+    isShowChart: false;
+    this.isChartCreated = false;
+    showChartButtonTag: "View";
+    userName: "";
+    URL: "";
+    repository:"";
+    company: "";
+    userDetails: {};
+    repoData: [];
+    followers: [];
+    followersDetails : new Map();
+    colorGenerator : {};
+    this.chartData.length = 0;
+    this.chartLabels.length = 0;
+    this.backgroundColor.length = 0;
+  }
+
+  fetchUser = function (form: any) {
+
+    this.resetData();
     this.isShowRepositories = false;
-    if(form.value.user != ""){
-      this.getUser(form.value.user).then((res)=>{
-        if(res.data.name == null){
+    this.showRepoButtonTag = "Show Repositories";
+    this.isShowChart = false;
+    this.showChartButtonTag = "View";
+
+    if (form.value.user != "") {
+      this.getUser(form.value.user).then((res) => {
+        if (res.data.name == null) {
           this.isInvalidUser = true;
           return;
         }
@@ -41,57 +88,94 @@ export class OperationComponent implements OnInit {
         this.URL = res.data.url;
         this.repository = res.data.public_repos;
         this.company = res.data.company;
-        if(this.company == null)
+
+        if (this.company == null)
           this.company = "Personal"
-      },(err)=>{
+
+          this.getRepoDetails().then((res) => {
+            this.repoData = res.data;
+          }, (err) => {
+            console.error(err);
+          })
+
+      }, (err) => {
         console.error(err);
       })
     }
   };
-  fetchFollowers = function(){
-      this.getFollowers().then((res)=>{
-        this.followers = res.data;
-        for(let follower of this.followers){
-          this.getUser(follower.login).then((res)=>{
-            this.followersDetails.set(follower.login,res.data);
-          },(err)=>{
-            console.log("User not found "+err);
-          })
-        }
-      },(err)=>{
-        console.error(err);
-      });
-  }
- 
-  fetchRepo = function(){
-      this.isShowRepositories = !this.isShowRepositories;
-      this.showRepoButtonTag = this.isShowRepositories ? "Hide Repositories" : "Show Repositories";
-      this.getRepoDetails().then((res)=>{
-        this.repoData = res.data;
-      },(err)=>{
-        console.error(err);
-      })
+  fetchFollowers = function () {
+    this.getFollowers().then((res) => {
+      this.followers = res.data;
+      for (let follower of this.followers) {
+        this.getUser(follower.login).then((res) => {
+          this.followersDetails.set(follower.login, res.data);
+        }, (err) => {
+          console.log("User not found " + err);
+        })
+      }
+    }, (err) => {
+      console.error(err);
+    });
   }
 
-  getUser = async function(user:string){
+  fetchRepo = function () {
+    this.isShowChart = false;
+    this.showChartButtonTag = "View";
+
+    this.isShowRepositories = !this.isShowRepositories;
+    this.showRepoButtonTag = this.isShowRepositories ? "Hide Repositories" : "Show Repositories";
+    if(!this.repoData){
+    this.getRepoDetails().then((res) => {
+      this.repoData = res.data;
+    }, (err) => {
+      console.error(err);
+    })
+  }
+  }
+
+  getUser = async function (user: string) {
     var rquestURI = `https://api.github.com/users/${user}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`;
     var api_call = await fetch(rquestURI);
     var data = await api_call.json();
     return { data };
   };
 
-  getRepoDetails = async function(){
+  getRepoDetails = async function () {
+    console.log(`${this.userDetails.repos_url}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`);
     var api_call = await fetch(`${this.userDetails.repos_url}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`);
     var data = await api_call.json();
     return { data };
   }
 
-  getFollowers = async function(){
+  getFollowers = async function () {
     var api_call = await fetch(`${this.userDetails.followers_url}?client_id=${this.CLIENT_ID}&client_secret=${this.CLIENT_SECRET}`)
     var data = await api_call.json();
-    return { data }; 
+    return { data };
   }
 
+  getChart = function () {
+    this.isShowRepositories = false;
+    this.showRepoButtonTag = "Show Repositories";
 
+    this.isShowChart = !this.isShowChart;
+    this.showChartButtonTag = this.isShowChart ? "Hide" : "View";
 
+      console.log("Is Chart Created : "+ this.isChartCreated);
+    if(!this.isChartCreated){
+    for(let repo of this.repoData){
+      this.chartData.push(repo.forks_count);
+      this.chartLabels.push(
+            repo.name
+      );
+      this.backgroundColor.push(this.colorGenerator.materialColor());
+    }
+    this.chartColors[0].backgroundColor = this.backgroundColor;
+  }
+  this.isChartCreated = true;
+  }
+  public chartOptions: any = {
+    responsive: true
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
 }
